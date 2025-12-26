f67dbce651fb3c03fa924ea3ccd8276e586af301
joelcodesalot <joelkabui98@gmail.com>
2025-12-26 14:46:04 +0300

    Refactor course exports to use imports

--- src/courses/index.js
+++ src/courses/index.js
@@ -1,9 +1,21 @@
-// src/courses/index.js
-export { htmlLessons } from "./html";
-export { pythonLessons } from "./lessons";
-export { clLessons } from "./clessons";
-export { cppLessons } from "./cpplessons";
-export { cssLessons } from "./css";
-export { goLessons } from "./golessons";
-export { sqlLessons } from "./sqllessons";
-export { rLessons } from "./Rlessons";
+// Import everything from the individual files
+import { htmlLessons } from "./html.js";
+import { cssLessons } from "./css.js";
+import { pythonLessons } from "./pythonlessons.js"; // Ensure this filename matches exactly!
+import { clLessons } from "./clessons.js";
+import { cppLessons } from "./cpplessons.js";
+import { golessons as goLessons } from "./golessons.js";
+import { sqllessons as sqlLessons } from "./sqllessons.js";
+import { Rlessons as rLessons } from "./Rlessons.js";
+
+// Export them all so App.jsx can see them
+export {
+  htmlLessons,
+  pythonLessons,
+  clLessons,
+  cppLessons,
+  cssLessons,
+  goLessons,
+  sqlLessons,
+  rLessons
+};
