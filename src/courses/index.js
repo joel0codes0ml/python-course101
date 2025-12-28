df5c96b7edaf83e785e9c9b0b7061fc18ca8bcc8
joelcodesalot <joelkabui98@gmail.com>
2025-12-29 00:47:23 +0300

    Update index.js

--- src/courses/index.js
+++ src/courses/index.js
@@ -1,21 +1,25 @@
 // src/courses/index.js
 
-import { htmlLessons } from "./html.js";
-import { cssLessons } from "./css.js";
-import { pythonLessons } from "./python.js";
-import { clLessons } from "./clessons.js";
-import { cppLessons } from "./cpplessons.js";
-import { goLessons } from "./golessons.js"; // Matches your export const goLessons
-import { sqllessons as sqlLessons } from "./sqllessons.js"; 
-import { Rlessons as rLessons } from "./Rlessons.js";
+import * as htmlMod from "./html.js";
+import * as cssMod from "./css.js";
+import * as pythonMod from "./python.js";
+import * as cMod from "./clessons.js";
+import * as cppMod from "./cpplessons.js";
+import * as goMod from "./golessons.js";
+import * as sqlMod from "./sqllessons.js";
+import * as rMod from "./Rlessons.js";
 
-export {
-  htmlLessons,
-  pythonLessons,
-  clLessons,
-  cppLessons,
-  cssLessons,
-  goLessons,
-  sqlLessons,
-  rLessons,
+// This helper function finds the data array inside each file 
+// regardless of how you capitalized the variable name.
+const getLessons = (mod) => {
+  return Object.values(mod).find(val => Array.isArray(val)) || [];
 };
+
+export const htmlLessons = getLessons(htmlMod);
+export const cssLessons = getLessons(cssMod);
+export const pythonLessons = getLessons(pythonMod);
+export const clLessons = getLessons(cMod);
+export const cppLessons = getLessons(cppMod);
+export const goLessons = getLessons(goMod);
+export const sqlLessons = getLessons(sqlMod);
+export const rLessons = getLessons(rMod);
