2fc70b99c9cf617c21ef7b066ab494ac73181e11
joelcodesalot <joelkabui98@gmail.com>
2025-12-29 00:43:09 +0300

    Update index.js

--- src/courses/index.js
+++ src/courses/index.js
@@ -1,25 +1,21 @@
 // src/courses/index.js
 
-import * as htmlModule from "./html.js";
-import * as cssModule from "./css.js";
-import * as pythonModule from "./python.js";
-import * as cModule from "./clessons.js";
-import * as cppModule from "./cpplessons.js";
-import * as goModule from "./golessons.js";
-import * as sqlModule from "./sqllessons.js";
-import * as rModule from "./Rlessons.js";
+import { htmlLessons } from "./html.js";
+import { cssLessons } from "./css.js";
+import { pythonLessons } from "./python.js";
+import { clLessons } from "./clessons.js";
+import { cppLessons } from "./cpplessons.js";
+import { goLessons } from "./golessons.js"; // Matches your export const goLessons
+import { sqllessons as sqlLessons } from "./sqllessons.js"; 
+import { Rlessons as rLessons } from "./Rlessons.js";
 
-// This helper function finds the first array in each file so you don't have to worry about variable names
-const extractLessons = (module) => {
-  const key = Object.keys(module).find(k => Array.isArray(module[k]));
-  return module[key] || [];
+export {
+  htmlLessons,
+  pythonLessons,
+  clLessons,
+  cppLessons,
+  cssLessons,
+  goLessons,
+  sqlLessons,
+  rLessons,
 };
-
-export const htmlLessons = extractLessons(htmlModule);
-export const cssLessons = extractLessons(cssModule);
-export const pythonLessons = extractLessons(pythonModule);
-export const clLessons = extractLessons(cModule);
-export const cppLessons = extractLessons(cppModule);
-export const goLessons = extractLessons(goModule);
-export const sqlLessons = extractLessons(sqlModule);
-export const rLessons = extractLessons(rModule);
