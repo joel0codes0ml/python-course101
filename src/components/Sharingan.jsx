684144672bd885bd6bcb4f3c09478fe33388ac56
joelcodesalot <joelkabui98@gmail.com>
2026-01-01 19:06:29 +0300

    Update Sharingan.jsx

--- src/components/Sharingan.jsx
+++ src/components/Sharingan.jsx
@@ -1,14 +1,69 @@
 import React from 'react';
 
-export default function Sharingan({ size = "w-12 h-12", animate = true }) {
+export default function Sharingan({ width = 80, height = 80, animate = true }) {
+  // Container style (The Red Eye)
+  const containerStyle = {
+    width: `${width}px`,
+    height: `${height}px`,
+    position: 'relative',
+    borderRadius: '50%',
+    backgroundColor: '#cc0000',
+    border: '2px solid black',
+    boxShadow: '0 0 20px rgba(204, 0, 0, 0.5)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden'
+  };
+
+  // The central black pupil
+  const pupilStyle = {
+    width: '25%',
+    height: '25%',
+    backgroundColor: 'black',
+    borderRadius: '50%',
+    zIndex: 10,
+    boxShadow: 'inset 0 0 5px rgba(255,255,255,0.2)'
+  };
+
   return (
-    <div className={`${size} relative rounded-full bg-[#cc0000] border-2 border-black shadow-[0_0_20px_rgba(204,0,0,0.5)] flex items-center justify-center ${animate ? 'animate-pulse' : ''}`}>
+    <div style={containerStyle}>
       {/* Pupil */}
-      <div className="w-1/4 h-1/4 bg-black rounded-full z-10 shadow-inner"></div>
-      {/* Tomoe Design */}
+      <div style={pupilStyle}></div>
+
+      {/* Tomoe Design (The 3 Commas) */}
       {[0, 120, 240].map((deg) => (
-        <div key={deg} className="absolute w-full h-full" style={{ transform: `rotate(${deg}deg)` }}>
-          <div className="absolute top-1 left-1/2 -translate-x-1/2 w-2 h-2 bg-black rounded-full after:content-[''] after:absolute after:w-1.5 after:h-2 after:border-l-2 after:border-black after:rounded-full after:-top-0.5 after:left-1"></div>
+        <div 
+          key={deg} 
+          style={{ 
+            position: 'absolute', 
+            width: '100%', 
+            height: '100%', 
+            transform: `rotate(${deg}deg)` 
+          }}
+        >
+          {/* The Tomoe Dot */}
+          <div style={{
+            position: 'absolute',
+            top: '10%',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            width: '15%',
+            height: '15%',
+            backgroundColor: 'black',
+            borderRadius: '50%'
+          }}>
+            {/* The Tomoe Tail (Comma Effect) */}
+            <div style={{
+              position: 'absolute',
+              width: '120%',
+              height: '150%',
+              borderLeft: '2px solid black',
+              borderRadius: '50%',
+              top: '-2px',
+              left: '50%'
+            }} />
+          </div>
         </div>
       ))}
     </div>
