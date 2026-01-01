f9c31dec4e6e552f3904fb762518ed8580947913
joelcodesalot <joelkabui98@gmail.com>
2026-01-01 14:12:35 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
-// src/main.jsx
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import Login from "./Login.jsx";
 import "./index.css";
+
+function Root() {
+  const [user, setUser] = useState(null);
+
+  // onLogin callback to set the user after login/register
+  const handleLogin = (loggedInUser) => {
+    setUser(loggedInUser);
+  };
+
+  // If no user, show Login page
+  if (!user) return <Login onLogin={handleLogin} />;
+
+  // If user exists, show main App
+  return <App user={user} />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
+
 
 
 
