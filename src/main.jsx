b5283c95ad353cf4882812a927f953da2ad4d5b7
joelcodesalot <joelkabui98@gmail.com>
2026-01-01 13:22:49 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { UserProvider } from "./context/UserContext";
+import App from "./App.jsx";
+import { UserProvider } from "./context/UserContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <UserProvider>
-    <App />
-  </UserProvider>
+  <React.StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </React.StrictMode>
 );
+
 
