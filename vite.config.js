66d6c849dd93f3f2f8b5424fa1534252b815b6b3
joelcodesalot <joelkabui98@gmail.com>
2026-01-01 16:30:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // ensure assets are served correctly on Vercel
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // You DO NOT want to externalize Firebase for browser builds
-      // external: []  <-- remove entirely
-    }
-  }
 });
-
-
-
